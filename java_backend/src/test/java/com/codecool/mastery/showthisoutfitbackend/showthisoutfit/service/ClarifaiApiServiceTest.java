82c2e8ac5a98969be3aea33c6fc360fb0b4b7e31
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-26 11:28:03 +0200

    Create ClarifaiApiServiceTest

---
+++ java_backend/src/test/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/service/ClarifaiApiServiceTest.java
@@ -0,0 +1,16 @@
+package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClarifaiApiServiceTest {
+
+    @Test
+    void getImageApparelLabels() {
+    }
+
+    @Test
+    void getImageDominantColor() {
+    }
+}
