b4d3eec39e9bcbd5ec8e3dff618a78de323d64d4
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-27 10:24:31 +0200

    Implement getImageDominantColorTest

--- java_backend/src/test/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/service/ClarifaiApiServiceTest.java
+++ java_backend/src/test/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/service/ClarifaiApiServiceTest.java
@@ -2,20 +2,20 @@ package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.service;
 
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.Label;
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated.clarifai.appareloutputs.*;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated.clarifai.coloroutputs.*;
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated.clarifai.inputs.InputsImage;
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.service.util.ClarifaiApiServiceUtil;
 import com.google.common.collect.Sets;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,22 +25,20 @@ import static org.mockito.Mockito.when;
 class ClarifaiApiServiceTest {
 
     private ClarifaiApiService clarifaiApiService;
-    private ClarifaiApiServiceUtil apiServiceUtil;
     private RestTemplate restTemplate;
     private InputsImage image;
 
     @BeforeEach
     public void init() {
         image = new InputsImage();
-        apiServiceUtil = new ClarifaiApiServiceUtil();
         restTemplate = mock(RestTemplate.class);
-        clarifaiApiService = new ClarifaiApiService(apiServiceUtil, restTemplate);
+        clarifaiApiService = new ClarifaiApiService(new ClarifaiApiServiceUtil(), restTemplate);
     }
 
     @Test
     void getImageApparelLabels() {
         BoundingBox boundingBox = new BoundingBox();
-        Label label = new Label(Arrays.asList("test"), boundingBox);
+        Label label = new Label(Collections.singletonList("test"), boundingBox);
         Set<Label> labels = Sets.newHashSet(label);
 
         ConceptsItem conceptsItem = new ConceptsItem();
@@ -59,13 +57,23 @@ class ClarifaiApiServiceTest {
     }
 
     @Test
-    void getImageDominantColor() {
+    void getImageDominantColorTest() {
+        ColorOutputs colorOutputsTest = getColorOutputsTest();
+
+        ResponseEntity<ColorOutputs> responseEntity = new ResponseEntity<>(colorOutputsTest, HttpStatus.ACCEPTED);
+
+        when(restTemplate.exchange(ArgumentMatchers.anyString(),
+                ArgumentMatchers.any(HttpMethod.class),
+                ArgumentMatchers.any(),
+                ArgumentMatchers.<Class<ColorOutputs>>any())).thenReturn(responseEntity);
+
+        assertThat("testColor3").isEqualTo(clarifaiApiService.getImageDominantColor(image));
     }
 
     private Outputs createTestOutput(ConceptsItem conceptsItem, BoundingBox boundingBox) {
 
         OutputsData outputsData = new OutputsData();
-        outputsData.setConcepts(Arrays.asList(conceptsItem));;
+        outputsData.setConcepts(Collections.singletonList(conceptsItem));
 
         RegionInfo regionInfo = new RegionInfo();
         regionInfo.setBoundingBox(boundingBox);
@@ -75,14 +83,50 @@ class ClarifaiApiServiceTest {
         regionsItem.setRegionInfo(regionInfo);
 
         OutputsData data = new OutputsData();
-        data.setRegions(Arrays.asList(regionsItem));
+        data.setRegions(Collections.singletonList(regionsItem));
 
         OutputsItem outputsItem = new OutputsItem();
         outputsItem.setData(data);
 
         Outputs outputs = new Outputs();
-        outputs.setOutputs(Arrays.asList(outputsItem));
+        outputs.setOutputs(Collections.singletonList(outputsItem));
 
         return outputs;
+    }
+
+    private ColorOutputs getColorOutputsTest() {
+        W3c color1 = new W3c();
+        color1.setName("testColor1");
+        W3c color2 = new W3c();
+        color2.setName("testColor2");
+        W3c color3 = new W3c();
+        color3.setName("testColor3");
+
+        Data data = Data.builder()
+                .colors(Arrays.asList(
+                        ColorsItem
+                                .builder()
+                                .w3c(color1)
+                                .value(1)
+                                .build(),
+                        ColorsItem
+                                .builder()
+                                .w3c(color2)
+                                .value(2)
+                                .build(),
+                        ColorsItem
+                                .builder()
+                                .w3c(color3)
+                                .value(3)
+                                .build()
+                        )
+                )
+                .build();
+
+        ColorOutputsItem item = new ColorOutputsItem();
+        item.setData(data);
+        ColorOutputs colorOutputs = new ColorOutputs();
+        colorOutputs.setOutputs(Collections.singletonList(item));
+        return colorOutputs;
     }
 }
