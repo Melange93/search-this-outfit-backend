034c03a1f40e9b243b2d88eb53c1f6a459fae0ce
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-08 09:38:46 +0200

    Delete the ugly webscraper

--- java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/contoller/ClothingController.java
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/contoller/ClothingController.java
@@ -5,13 +5,9 @@ import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.ChosenIte
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.Label;
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated.clarifai.inputs.InputsImage;
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.service.ClothingService;
-import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.util.WebScraper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 @CrossOrigin
@@ -20,9 +16,6 @@ public class ClothingController {
 
     @Autowired
     private ClothingService clothingService;
-
-    @Autowired
-    private WebScraper webScraper;
 
     @PostMapping("/picture/upload")
     public Set<Label> getUploadImageLabels(@RequestBody InputsImage base64Image) {
